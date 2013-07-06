df50d8aef9b619f9c2415fc9e5d8a4bf73cbc842
Boy van Amstel <boy@boyvanamstel.nl>
2013-07-06 10:52:17 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackathonbuza::Application.config.secret_key_base = 'c928018ec0492987daf83619422f41a388bc688b4d75771001ad4f46831e157de43f72c8d1c686d1ba4cd8939acce147323fc03b4d5536b4f3dc0198d06d8952'
