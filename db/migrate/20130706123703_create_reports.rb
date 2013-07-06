611b7a9a01dab17f4c5afb589c3f8747ed064133
Raldenhoven <robin.aldenhoven@tamtam.nl>
2013-07-06 15:39:13 +0200

    reports cron finished

--- db/migrate/20130706123703_create_reports.rb
+++ db/migrate/20130706123703_create_reports.rb
@@ -1,7 +1,8 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-      t.reference :country
+      t.references :country
+      t.string :algemeen
       t.string :actueel
       t.string :terrorisme
       t.string :criminaliteit
